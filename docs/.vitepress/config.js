403bd305c62ddb3dde1052e6a40b1656e794ae27
zeMing <duzm@chinachdu.com>
2023-12-23 14:09:20 +0800

    feat:添加百度统计

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,16 @@ export default {
   head: [
     ['link', { rel: 'icon', href: '/img/logo.svg' }],
     ['link', { rel: 'stylesheet', href: '/style/index.css' }],
-    ['script', { src: '/js/baidu-analytics.js' }],
+    // ['script', { src: '/js/baidu-analytics.js' }],
+    ['script', {}, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?caeddcfe6f8c0b222474f7907aedb4a1"
+        var s = document.getElementsByTagName("script")[0]
+        s.parentNode.insertBefore(hm, s)
+      })()
+    `],
   ],
   themeConfig: {
     nav, // 导航栏配置

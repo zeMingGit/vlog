1494d28bdda828b74740105dd123724d2f6ce6b5
zeMing <duzm@chinachdu.com>
2024-01-08 16:13:44 +0800

    fix:路由试改造

--- docs/.vitepress/router/vue2.js
+++ docs/.vitepress/router/vue2.js
@@ -1,11 +1,28 @@
 // 作者: zeMing
+import fs from 'fs'
+import path from 'path'
+const currentDir = path.dirname(__dirname)
+const grandfatherDir = path.dirname(currentDir)
+const siblingFolderName = 'doc/vue2'
+const siblingFolderPath = path.join(grandfatherDir, siblingFolderName)
+const files = fs.readdirSync(siblingFolderPath)
+
+let text = new Map([
+  ['index', '概述'],
+  ['precision', 'Number精度问题'],
+])
+let items = files.map(item => {
+  const name = item.split('.')[0]
+  return {
+    text: text.get(name) || name,
+    link: `/doc/vue2/${name}`
+  }
+})
+
 const vue2 ={
   text: 'vue2',
   collapsed: false,
-  items: [
-    { text: '概述', link: '/doc/vue2/' },
-    { text: 'Number精度问题', link: '/doc/vue2/precision' },
-  ]
+  items,
 }
 
 export default vue2

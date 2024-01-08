d9369448f5afea43bc2a7b359b8dc11af779ec1e
zeMing <duzm@chinachdu.com>
2024-01-08 22:22:04 +0800

    feat:搜索完整

--- docs/.vitepress/router/vue2.js
+++ docs/.vitepress/router/vue2.js
@@ -20,3 +20,17 @@ const vue2 ={
 }
 
 export default vue2
+
+
+
+// // 作者: zeMing
+// const vue2 ={
+//   text: 'vue2',
+//   collapsed: false,
+//   items: [
+//     { text: '概述', link: '/doc/vue2/' },
+//     { text: 'Number精度问题', link: '/doc/vue2/precision' },
+//   ]
+// }
+
+// export default vue2
--- docs/.vitepress/script/Search.js
+++ docs/.vitepress/script/Search.js
@@ -39,9 +39,52 @@ const search = {
 const searchAlgolia = {
   provider: 'algolia',
   options: {
-    appId: 'R0JRESJRK0',
-    apiKey: '96e84e24f9682766082d30a2f8294461',
-    indexName: 'vlog'
+    appId: '2HEGWEY7SW',
+    apiKey: '2e4d854dceb221dedc1ecb0b397a8373',
+    indexName: 'vlogxiao',
+    locales: {
+      zh: {
+        placeholder: '搜索文档',
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            searchBox: {
+              resetButtonTitle: '清除查询条件',
+              resetButtonAriaLabel: '清除查询条件',
+              cancelButtonText: '取消',
+              cancelButtonAriaLabel: '取消'
+            },
+            startScreen: {
+              recentSearchesTitle: '搜索历史',
+              noRecentSearchesText: '没有搜索历史',
+              saveRecentSearchButtonTitle: '保存至搜索历史',
+              removeRecentSearchButtonTitle: '从搜索历史中移除',
+              favoriteSearchesTitle: '收藏',
+              removeFavoriteSearchButtonTitle: '从收藏中移除'
+            },
+            errorScreen: {
+              titleText: '无法获取结果',
+              helpText: '你可能需要检查你的网络连接'
+            },
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+              searchByText: '搜索提供者'
+            },
+            noResultsScreen: {
+              noResultsText: '无法找到相关结果',
+              suggestedQueryText: '你可以尝试查询',
+              reportMissingResultsText: '你认为该查询应该有结果？',
+              reportMissingResultsLinkText: '点击反馈'
+            }
+          }
+        }
+      }
+    }
   }
 }
 

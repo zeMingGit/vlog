4c4d12a47b23ca576c4ba0b5e208d096b4a1a26f
zeMing <duzm@chinachdu.com>
2024-01-11 11:47:27 +0800

    feat:重构配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,10 +1,9 @@
-// import fs from 'fs'
-// import path from 'path'
+import fs from 'fs'
+import path from 'path'
 import { defineConfig } from 'vitepress'
 import nav from './script/Nav'
 import sidebar from './script/Sidebar'
 import search from './script/Search'
-import lastUpdated from './script/LastTime'
 
 export default defineConfig({
   lang: 'zh-CN',
@@ -29,19 +28,16 @@ export default defineConfig({
   themeConfig: {
     nav, // 导航栏配置
     sidebar, // 侧边栏配置
-    search, // 搜索配置
-    lastUpdated, // 允许自定义最后更新文本和日期格式
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: '2HEGWEY7SW',
+        apiKey: '2e4d854dceb221dedc1ecb0b397a8373',
+        indexName: 'vlogxiao',
+        locales: { ...search },
+      }
+    }, // 搜索配置
     logo: { light: "/img/logo.svg", dark: "/img/logoFFF.svg" },
-    // copyCode: {
-    //   buttonText: '复制代码', // 在这里更改按钮文字
-    //   errorText: '复制失败', // 更改复制失败时的提示文字
-    //   successText: '已复制' // 更改成功复制时的提示文字
-    // },
-    outline: [2, 4], // 设置标题的大纲深度，即显示到哪一级标题
-    outlineTitle: '本页目录', // 设置大纲的标题
-    sidebarMenuLabel: '菜单',
-    returnToTopLabel: '返回顶部',
-    docFooter: { prev: '上一页', next: '下一页' }, // 自定义上一个和下一个链接上方显示的文本
     socialLinks: [
       { icon: 'github', link: 'https://github.com/2439340964' },
     ],
@@ -52,15 +48,25 @@ export default defineConfig({
     },
     footer: {
       message: '欢迎加入我们，一起来完善小棱镜文档吧',
-      copyright: copyright(),
+      copyright: `Copyright© 2023-${new Date().getFullYear()} zeMing`,
     },
+    docFooter: { prev: '上一页', next: '下一页' }, // 自定义上一个和下一个链接上方显示的文本
+    outline: { label: '页面导航' }, // 设置标题的大纲深度，即显示到哪一级标题
+    lastUpdated: {
+      text: '更新时间',
+      formatOptions: {
+        dateStyle: 'short',
+        // dateSty用于指定日期的样式，可选值包括 'full'、'long'、'medium' 和 'short'
+        timeStyle: 'medium',
+        // timeStyle用于指定时间的样式，可选值包括 'full'、'long'、'medium' 和 'short'
+      }
+    }, // 允许自定义最后更新文本和日期格式
+    langMenuLabel: '多语言',
+    returnToTopLabel: '返回顶部',
+    outlineTitle: '本页目录', // 设置大纲的标题
+    sidebarMenuLabel: '菜单',
     darkModeSwitchLabel: '切换外观',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
   }
 })
-
-
-function copyright(): string {
-  let endYear: number | string = new Date().getFullYear()
-  endYear = endYear > 2023 ? `2023-${endYear}` : 2023
-  return `Copyright©${endYear} zeMing`
-}
--- docs/.vitepress/script/Search.ts
+++ docs/.vitepress/script/Search.ts
@@ -37,56 +37,48 @@ const searchBox = {
 }
 
 const search = {
-  provider: 'algolia' as const,
-  options: {
-    appId: '2HEGWEY7SW',
-    apiKey: '2e4d854dceb221dedc1ecb0b397a8373',
-    indexName: 'vlogxiao',
-    locales: {
-      // root是默认的本地化文本选项
-      root: {
-        // 指定根目录下的本地化文本。
-        placeholder: '搜索文档',
-        translations: {
-          button: {
-            buttonText: '搜索文档',
-            buttonAriaLabel: '搜索文档'
-          },
-          modal: {
-            searchBox: {
-              resetButtonTitle: '清除查询条件',
-              resetButtonAriaLabel: '清除查询条件',
-              cancelButtonText: '取消',
-              cancelButtonAriaLabel: '取消'
-            },
-            noResultsText: '无法找到相关结果',
-            resetButtonTitle: '清除查询条件',
-            footer: {
-              selectText: '选择',
-              navigateText: '切换',
-              closeText: '关闭',
-              searchByText: '搜索提供者'
-            },
-            errorScreen: {
-              titleText: '无法获取结果',
-              helpText: '你可能需要检查你的网络连接'
-            },
-            startScreen: {
-              recentSearchesTitle: '搜索历史',
-              noRecentSearchesText: '没有搜索历史',
-              saveRecentSearchButtonTitle: '保存至搜索历史',
-              removeRecentSearchButtonTitle: '从搜索历史中移除',
-              favoriteSearchesTitle: '收藏',
-              removeFavoriteSearchButtonTitle: '从收藏中移除'
-            },
-            noResultsScreen: {
-              noResultsText: '无法找到相关结果',
-              suggestedQueryText: '你可以尝试查询',
-              reportMissingResultsText: '你认为该查询应该有结果？',
-              reportMissingResultsLinkText: '点击反馈'
-            },
-          }
-        }
+  // root是默认的本地化文本选项
+  root: {
+    // 指定根目录下的本地化文本。
+    placeholder: '搜索文档',
+    translations: {
+      button: {
+        buttonText: '搜索文档',
+        buttonAriaLabel: '搜索文档'
+      },
+      modal: {
+        searchBox: {
+          resetButtonTitle: '清除查询条件',
+          resetButtonAriaLabel: '清除查询条件',
+          cancelButtonText: '取消',
+          cancelButtonAriaLabel: '取消'
+        },
+        noResultsText: '无法找到相关结果',
+        resetButtonTitle: '清除查询条件',
+        footer: {
+          selectText: '选择',
+          navigateText: '切换',
+          closeText: '关闭',
+          searchByText: '搜索提供者'
+        },
+        errorScreen: {
+          titleText: '无法获取结果',
+          helpText: '你可能需要检查你的网络连接'
+        },
+        startScreen: {
+          recentSearchesTitle: '搜索历史',
+          noRecentSearchesText: '没有搜索历史',
+          saveRecentSearchButtonTitle: '保存至搜索历史',
+          removeRecentSearchButtonTitle: '从搜索历史中移除',
+          favoriteSearchesTitle: '收藏',
+          removeFavoriteSearchButtonTitle: '从收藏中移除'
+        },
+        noResultsScreen: {
+          noResultsText: '无法找到相关结果',
+          suggestedQueryText: '你可以尝试查询',
+          reportMissingResultsText: '你认为该查询应该有结果？',
+          reportMissingResultsLinkText: '点击反馈'
+        },
       }
     }
   }

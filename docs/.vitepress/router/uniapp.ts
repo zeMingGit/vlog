206152c69ab0d134afa5cbc73905c6331226b9ba
zeMing <duzm@chinachdu.com>
2024-01-16 11:49:50 +0800

    feat:uniapp-文件上传

--- docs/.vitepress/router/uniapp.ts
+++ docs/.vitepress/router/uniapp.ts
@@ -8,6 +8,7 @@ const uniapp = {
     { text: 'npm获取命令行参数', link: '/get-params' },
     { text: '生成canvas进行电子签名', link: '/electronic-signature' },
     { text: '微信小程序获取当前位置', link: '/get-location' },
+    { text: '文件的下载保存', link: '/save-path' },
   ]
 }
 

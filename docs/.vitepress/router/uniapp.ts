fd80751d62084ccc5e8248217ec681b061e080f2
zeMing <duzm@chinachdu.com>
2024-01-17 15:35:04 +0800

    feat:uniapp-webview栏

--- docs/.vitepress/router/uniapp.ts
+++ docs/.vitepress/router/uniapp.ts
@@ -9,6 +9,7 @@ const uniapp = {
     { text: '生成canvas进行电子签名', link: '/electronic-signature' },
     { text: '微信小程序获取当前位置', link: '/get-location' },
     { text: '文件的下载保存', link: '/save-path' },
+    { text: 'webView拓展使用', link: '/web-view' },
   ]
 }
 

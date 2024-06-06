d346e7a76e7e890a68b396bd11023edaac8e2d8d
panmin <panmin05@chinasoftinc.com>
2024-06-06 17:08:31 +0800

    feat: 新增prettier.md

--- docs/.vitepress/router/vue2.ts
+++ docs/.vitepress/router/vue2.ts
@@ -8,29 +8,11 @@ const vue2 ={
   items: [
     { text: '概述', link: '/' },
     { text: 'Number精度问题', link: '/precision' },
+    { text: '格式化工具Prettier', link: '/prettier' },
   ]
 }
 
 export default vue2
-
-// let text = new Map([
-//   ['index', '概述'],
-//   ['precision', 'Number精度问题'],
-// ])
-// const items = readdirFilesV2({ path: 'docs/src/vue2', }).map((pkg) => {
-//   return {
-//     text: text.get(pkg),
-//     link: `/src/vue2/${pkg}`
-//   }
-// })
-
-// const vue2 ={
-//   text: 'vue2',
-//   collapsed: false,
-//   items,
-// }
-
-// export default vue2
 
 
 

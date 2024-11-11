c1a2c98c965fa247b58422926cfe288a74bf6f4f
zeMing <2439340964@qq.com>
2024-11-11 16:09:11 +0800

    fix:ts路由配置问题

--- .vitepress/router/vue.ts
+++ .vitepress/router/vue.ts
@@ -4,8 +4,7 @@ const vue = {
   base: '/vue3',
   items: [
     { text: 'vue3概述', link: '/' },
-    // { text: 'vue2概述', link: '/index_v2' },
-    { text: 'TypeScript', link: '/typescript' },
+    { text: 'TypeScript', link: '/typeScript' },
     { text: 'SHA 256', link: '/sha256' },
     { text: 'Number精度问题', link: '/precision' },
     { text: '格式化工具Prettier', link: '/prettier' },

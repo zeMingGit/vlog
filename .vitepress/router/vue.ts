51212fb6580a505f759ec5803e00a856ac12b534
zeMing <2439340964@qq.com>
2024-11-11 16:02:59 +0800

    feat:v3-ts栏目

--- .vitepress/router/vue.ts
+++ .vitepress/router/vue.ts
@@ -5,6 +5,7 @@ const vue = {
   items: [
     { text: 'vue3概述', link: '/' },
     // { text: 'vue2概述', link: '/index_v2' },
+    { text: 'TypeScript', link: '/typescript' },
     { text: 'SHA 256', link: '/sha256' },
     { text: 'Number精度问题', link: '/precision' },
     { text: '格式化工具Prettier', link: '/prettier' },
